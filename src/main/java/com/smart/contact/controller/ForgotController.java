e111481bc1aaeee6fee3bd0830ba1687a73b1fdd
faizan <faizanahmed2710@gmail.com>
2022-03-21 12:15:01 +0530

    revert back changes

--- src/main/java/com/smart/contact/controller/ForgotController.java
+++ src/main/java/com/smart/contact/controller/ForgotController.java
@@ -2,6 +2,8 @@ package com.smart.contact.controller;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,31 +11,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotController {
+
 	
+
 	Random random = new Random(1000);
 
 	// email id form open handler
 	@RequestMapping("/forgot")
 	public String openEmailForm() {
-		
+
 		return "forgot_email_form";
 	}
-	
+
 	@PostMapping("/send-otp")
-	public String sendOTP(@RequestParam ("email") String email) {
-		
-		System.out.println("EMAIL :- " +email);
-		
+	public String sendOTP(@RequestParam("email") String email, HttpSession session) {
+
+		System.out.println("EMAIL :- " + email);
+
 		// generating otp of 4 digits
-		
-		
-		
+
 		int otp = random.nextInt(999999);
+
+		System.out.println("OTP :- " + otp);
+
+		// code for send OTP to email..
+
 		
-		System.out.println("OTP :- " +otp);
+
+			return "verify_otp";
+
 		
-		// write code for send OTP to email..
-		
-		return "verify_otp";
+
 	}
 }
